23b589d39089f1d90e646dcd92dd97cf19e3a962
Sarvan-PC\Sarvan <dksarvan@gmail.com>
2017-08-14 18:33:47 +0530

    testng commit

---
+++ sunbird/src/test/java/com/org/sunbird/openWeb.java
@@ -0,0 +1,33 @@
+package com.org.sunbird;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+
+public class openWeb {	
+ 
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Inside BeforeMethod");
+  }
+
+  @Test
+  public void Test1() {
+	  System.out.println("Inside Test");
+  }
+  
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("Inside AfterMethod");
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+}
