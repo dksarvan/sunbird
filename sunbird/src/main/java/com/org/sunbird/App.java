b877c511b96ea32433d1c44cd8d507ff2a157027
sarvan <dksarvan@gmail.com>
2018-02-10 15:18:53 +0530

    cleared errors

--- sunbird/src/main/java/com/org/sunbird/App.java
+++ sunbird/src/main/java/com/org/sunbird/App.java
@@ -6,8 +6,10 @@ package com.org.sunbird;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Mithun!" );
+    public static void main( String[] args ) {
+    
+
+        System.out.println( "Hello Sarvan!" );
+
     }
 }
